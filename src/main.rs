dbeee1169757e2b7da2143f375d4f4ad0e62999a
Pascal Fries <pascalf@atix.de>
2023-03-12 21:00:15 +0100

    feat: implement simple scaler

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use std::sync::Arc;
 use tokio::{
     net::{TcpListener, TcpStream},
     sync::Notify,
-    time::{self, Duration},
+    time::Duration,
 };
 
 #[tokio::main]
-async fn main() -> Result<()>{
+async fn main() -> Result<()> {
     let listen_address = "0.0.0.0:3000";
     let backend_address = "127.0.0.1:8000";
 
@@ -18,7 +18,7 @@ async fn main() -> Result<()>{
 
     let backend_unavailable = Arc::new(Notify::new());
     let backend_available = Arc::new(Notify::new());
-    
+
     {
         let backend_unavailable = backend_unavailable.clone();
         let backend_available = backend_available.clone();
@@ -26,13 +26,16 @@ async fn main() -> Result<()>{
             loop {
                 backend_unavailable.notified().await;
                 println!("Got a request to a backend that is unreachable. Trying to scale up.");
-                time::sleep(Duration::from_millis(1500)).await;
+                // TODO: implement proper error handling
+                scaler::scale_deploy("wasm-spin", 1, Duration::from_secs(10))
+                    .await
+                    .expect("ffffff");
                 backend_available.notify_waiters();
                 println!("Backend is up again.");
             }
         });
     }
-    
+
     // handle incoming connections
     while let Ok((ingress, _)) = listener.accept().await {
         let backend_unavailable = backend_unavailable.clone();
@@ -44,16 +47,16 @@ async fn main() -> Result<()>{
                     Ok(backend) => {
                         proxy_connection(ingress, backend).await;
                         break;
-                    },
+                    }
                     Err(_) => {
                         backend_unavailable.notify_one();
                         backend_available.notified().await;
-                    },
+                    }
                 };
             }
         });
     }
-    
+
     Ok(())
 }
 
@@ -61,9 +64,9 @@ async fn proxy_connection(mut ingress: TcpStream, mut backend: TcpStream) {
     match tokio::io::copy_bidirectional(&mut ingress, &mut backend).await {
         Ok((bytes_to_backend, bytes_from_backend)) => {
             println!("Connection ended gracefully ({bytes_to_backend} bytes from client, {bytes_from_backend} bytes from server)");
-        },
-        Err(err) => {
-            println!("Error while proxying: {}", err);
-        },
+        }
+        Err(e) => {
+            println!("Error while proxying: {e}");
+        }
     }
 }
--- src/scaler.rs
+++ src/scaler.rs
@@ -1,3 +1,44 @@
-pub fn hello() {
-    println!("Hello!");
+use anyhow::{Context, Result};
+use k8s_openapi::api::{apps::v1::Deployment, autoscaling::v1::Scale};
+use kube::{
+    api::{Api, Patch, PatchParams},
+    Client,
+};
+use serde_json::json;
+use tokio::time::{self, Duration};
+
+pub async fn scale_deploy(name: &str, replicas: i32, ready_timeout: Duration) -> Result<()> {
+    let client = Client::try_default().await?;
+    let deploy: Api<Deployment> = Api::default_namespaced(client);
+    let scale: Scale = serde_json::from_value(json!({
+        "apiVersion": "autoscaling/v1",
+        "kind": "Scale",
+        "spec": {"replicas": replicas }
+    }))?;
+
+    // do a server-side apply where "sero" is the fieldManager
+    let patch = Patch::Apply(&scale);
+    let params = PatchParams::apply("sero");
+    deploy.patch_scale(name, &params, &patch).await?;
+
+    // wait until enough replicas are ready, this will periodically poll the apiserver
+    // after a specified timeout, cancel and error
+    let mut interval = time::interval(Duration::from_millis(100));
+    time::timeout(ready_timeout, async move {
+        while deploy
+            .get_status(name)
+            .await?
+            .status
+            .context("Deployment has no status o.O")?
+            .ready_replicas
+            .context("Deployment does not know about its ready replicas o.O")?
+            < replicas
+        {
+            interval.tick().await;
+        }
+        Result::<()>::Ok(())
+    })
+    .await??;
+
+    Ok(())
 }
